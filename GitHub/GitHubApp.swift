12e0d47c4023cd97dbf6ac76b6ea8cebfe228dc1
Nyan Lin Tun <nyanlynntunn@gmail.com>
2025-05-14 16:44:50 +0800

    Init project

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Nyan Lin Tun on 14/5/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
