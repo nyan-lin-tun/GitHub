a910955ef09fdbac5566dca513b1c6f3e5512bf3
Nyan Lin Tun <nyanlynntunn@gmail.com>
2025-05-15 23:30:31 +0800

    Webview for repository

--- GitHub/Views/WebView/WebView.swift
+++ GitHub/Views/WebView/WebView.swift
@@ -5,4 +5,19 @@
 //  Created by Nyan Lin Tun on 15/5/25.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        WKWebView()
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        if webView.url != url {
+            webView.load(URLRequest(url: url))
+        }
+    }
+}
