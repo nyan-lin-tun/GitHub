5c367c46d6c330618baf493d0af8db6f133ddc41
Nyan Lin Tun <nyanlynntunn@gmail.com>
2025-05-16 10:10:28 +0800

    GitHubRepository for domain layer

--- GitHub/Models/Domain/GitHubRepository.swift
+++ GitHub/Models/Domain/GitHubRepository.swift
@@ -1,5 +1,5 @@
 //
-//  GitHubRepositoryResponse.swift
+//  GitHubRepository.swift
 //  GitHub
 //
 //  Created by Nyan Lin Tun on 16/5/25.
@@ -8,22 +8,30 @@
 
 import Foundation
 
-struct GitHubRepository: Codable, Equatable, Identifiable {
-    enum CodingKeys: String, CodingKey {
-        case id
-        case name
-        case language
-        case stargazersCount = "stargazers_count"
-        case description
-        case fork
-        case htmlUrl = "html_url"
-    }
-    
+struct GitHubRepository: Equatable, Identifiable {
     let id: Int
-    let name: String?
-    let language: String?
-    let stargazersCount: Int?
-    let description: String?
-    let fork: Bool?
-    let htmlUrl: String?
+    let name: String
+    let language: String
+    let starsCount: Int
+    let description: String
+    let fork: Bool
+    let htmlUrl: String
+    
+    init(
+        id: Int,
+        name: String,
+        language: String,
+        starsCount: Int,
+        description: String,
+        fork: Bool,
+        htmlUrl: String
+    ) {
+        self.id = id
+        self.name = name
+        self.language = language
+        self.starsCount = starsCount
+        self.description = description
+        self.fork = fork
+        self.htmlUrl = htmlUrl
+    }
 }
