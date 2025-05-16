3e8c6acb7aeade7290b8fc98fea2cf08e000b956
Nyan Lin Tun <nyanlynntunn@gmail.com>
2025-05-16 10:11:53 +0800

    Create mapper for github repository

--- GitHub/Models/Mapper/GitHubRepositoryMapper.swift
+++ GitHub/Models/Mapper/GitHubRepositoryMapper.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct GitHubRepositoryMapper: ModelMapper {
+    typealias Input = GitHubRepositoryResponse
+    typealias Output = GitHubRepository
+    
+    func map(input: GitHubRepositoryResponse) -> GitHubRepository {
+        return .init(
+            id: input.id,
+            name: input.name ?? "",
+            language: input.language ?? "",
+            starsCount: input.stargazersCount ?? 0,
+            description: input.description ?? "",
+            fork: input.description ?? false,
+            htmlUrl: input.htmlUrl ?? ""
+        )
+    }
+}
