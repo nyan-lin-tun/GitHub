973eb0cf7580e8fc164f901c0bc143d5f1421c60
Nyan Lin Tun <nyanlynntunn@gmail.com>
2025-05-16 14:22:58 +0800

    Add unit tests for usecase

--- GitHubTests/Services/GitHubAPIServiceUseCaseTests.swift
+++ GitHubTests/Services/GitHubAPIServiceUseCaseTests.swift
@@ -5,31 +5,53 @@
 //  Created by Nyan Lin Tun on 16/5/25.
 //
 
+@testable import GitHub
 import XCTest
 
 final class GitHubAPIServiceUseCaseTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    private var mockRepo: MockGitHubRepository!
+    private var useCase: GitHubAPIServiceUseCase!
+    
+    override func setUp() {
+        super.setUp()
+        mockRepo = MockGitHubRepository()
+        useCase = GitHubAPIServiceUseCase(repository: mockRepo)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        mockRepo = nil
+        useCase = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testFetchUsers_CallsRepositoryAndReturnsData() async throws {
+        let users = try await useCase.fetchUsers(since: nil)
+        XCTAssertTrue(mockRepo.fetchUsersCalled)
+        XCTAssertEqual(users.count, 1)
+        XCTAssertEqual(users.first?.id, 123)
+        XCTAssertEqual(users.first?.login, "dummyUser")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testFetchUserDetail_CallsRepositoryAndReturnsDetail() async throws {
+        let detail = try await useCase.fetchUserDetail(username: "testUser")
+        XCTAssertTrue(mockRepo.fetchDetailCalled)
+        XCTAssertEqual(detail.login, "testUser")
+        XCTAssertEqual(detail.name, "Dummy Name")
+        XCTAssertEqual(detail.followers, 5)
+        XCTAssertEqual(detail.following, 3)
     }
-
+    
+    func testFetchUserRepositories_CallsRepositoryAndFiltersForks() async throws {
+        let repos = try await useCase.fetchUserRepositories(username: "testUser", page: 1, perPage: 10)
+        XCTAssertTrue(mockRepo.fetchReposCalled)
+        XCTAssertEqual(repos.count, 1)
+        let repo = repos[0]
+        XCTAssertEqual(repo.id, 456)
+        XCTAssertEqual(repo.name, "dummy-repo")
+        XCTAssertEqual(repo.language, "Swift")
+        XCTAssertEqual(repo.starsCount, 42)
+        XCTAssertEqual(repo.description, "A dummy repo")
+        XCTAssertFalse(repo.fork)
+        XCTAssertEqual(repo.htmlUrl, "https://github.com/dummyUser/dummy-repo")
+    }
 }
